ef77927f13d699a29e8d52fcd78c3655be460f3d
Victor Gea <victorluisgea@gmail.com>
2019-07-05 11:29:15 +0200

    Update pb_to_gcs_diffoutputs.py

--- pb_to_gcs_diffoutputs.py
+++ pb_to_gcs_diffoutputs.py
@@ -3,7 +3,9 @@ import logging
                                                                                                                                                                                                                                                 
 import apache_beam as beam                                                                                                                                                                                                                      
 from apache_beam import pvalue                                                                                                                                                                                                                  
-from apache_beam.options.pipeline_options import PipelineOptions                                                                                                                                                                                
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import SetupOptions
+
 from apache_beam.io.gcp.pubsub import ReadFromPubSub                                                                                                                                                                                            
 from apache_beam.io import WriteToText                                                                                                                                                                                                          
 from apache_beam import window                                                                                                                                                                                                                  
@@ -21,7 +23,8 @@ def run(argv=None):
     pipeline_args.extend(['--project={}'.format(project)])                                                                                                                                                                                      
                                                                                                                                                                                                                                                 
     pipeline_options = PipelineOptions(pipeline_args)                                                                                                                                                                                           
-                                                                                                                                                                                                                                                
+    pipeline_options.view_as(SetupOptions).save_main_session = True
+    
     class DiffOutputsFn(beam.DoFn):                                                                                                                                                                                                             
         # These tags will be used to tag the outputs of this DoFn.                                                                                                                                                                              
         OUTPUT_TAG_CS_BOOKINGS = 'tag_cs_bookings'                                                                                                                                                                                              
@@ -38,12 +41,10 @@ def run(argv=None):
     
     def string_join(elements):                                                                                                                                                                                                                  
         return str('\n'.join(elements))                                                                                                                                                                                                         
-                                                                                                                                                                                                                                                
-"https://github.com/apache/beam/blob/master/sdks/python/apache_beam/examples/cookbook/multiple_output_pardo.py"                                                                                
-                                                                                                                                                                                                                                                
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
                                                                                                                                                                                                                                                 
     with beam.Pipeline(options=pipeline_options) as p:                                                                                                                                                                                          
-        #current_topic = known_args.topic_prefix                                                                                                                                                                                                
+
         diff_outputs = (p | "ReadTopic" >>                                                                                                                                                                                                      
                         beam.io.ReadFromPubSub(topic=default_topic) |                                                                                                                                                                           
                         "SplitOutputs" >> beam.ParDo(DiffOutputsFn()).with_outputs(DiffOutputsFn.OUTPUT_TAG_CS_BOOKINGS,DiffOutputsFn.OUTPUT_TAG_USERS))                                                                                        
@@ -52,7 +53,7 @@ def run(argv=None):
          | "Combine_cs_bookings" >> beam.CombineGlobally(string_join).without_defaults()                                                                                                                                                        
          | "WriteGCSCommon_cs_bookings" >> WriteToText(file_path_prefix=default_bucket+'cs_bookings/',file_name_suffix='cs_booking_YYYYMMDDHH'))                                                                                                               
         
-        users_p = (diff_outputs.tag_cs_bookings                                                                                                                                                                                                 
+        users_p = (diff_outputs.tag_users                                                                                                                                                                                                 
          | "Windowing_users" >> beam.WindowInto(window.FixedWindows(WINDOW_LENGTH))                                                                                                                                                             
          | "Combine_users" >> beam.CombineGlobally(string_join).without_defaults()                                                                                                                                                              
          | "WriteGCSCommon_users" >> WriteToText(file_path_prefix=default_bucket+'users/',file_name_suffix='users_YYYYMMDDHH'))                                                                                                                       
